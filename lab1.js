8f900d09ca7842c06894ccf896cf5e7e06d8f477
Lisa Miller <lisayevonne@outlook.com>
2020-04-15 16:21:03 -0400

    update

--- lab1.js
+++ lab1.js
@@ -1,9 +1,12 @@
-var list = $('.userlist');
-$.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts', function(response){
+$("#button1").click(function(){
+  $.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts', function(response){
     console.log(response);
     $.each(response.data, function(x)){
         var li = $('<li></li>');
         li.text(this.email);
         list.append(li);
-    });
+    };
 })
+});
+
+
