8481538e0eb55535f64036a9cd9a99d9fe7bc688
Lisa Miller <lisayevonne@outlook.com>
2020-04-18 19:40:22 -0400

    exercise 3 update

--- lab3.js
+++ lab3.js
@@ -1,7 +1,17 @@
-let makeAllCaps = function () {
+$("button").click(function() {
+  	var list = $('#target');    
+    var li = $('<li></li>');
+    	li.text($('input').val().toUpperCase());
+        list.append(li);
+});
 
-}
 
-let sortWords = function () {
-	
-}
+$("button").click(function () {
+	var mylist = $('#target');
+	var listitems = mylist.children('li').get();
+	listitems.sort(function(a, b) {
+   	return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+})
+$.each(listitems, function(idx, itm) { mylist.append(itm); });
+
+})
